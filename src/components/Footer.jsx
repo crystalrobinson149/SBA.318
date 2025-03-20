15dc2ac50dc6e33d987659fc866620806bdb9a59
crystalrobinson149 <crystalrobinson149@gmail.com>
2025-03-20 07:54:00 -0400

    add new

---
+++ src/components/Footer.jsx
@@ -0,0 +1,18 @@
+function Footer() {
+    return (
+        <footer style={styles}>
+            <h3>Money money money... mon-ey!</h3>
+            {/* <a href=""></a>
+            <a href=""></a> */}
+        </footer>
+    );
+}
+
+const styles = {
+    display: "flex",
+    justifyContent: "space-around", 
+    backgroundColor: "turquoise"
+}
+
+export default Footer;
+
